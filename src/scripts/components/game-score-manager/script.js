a5b928e4927afc887221fb34ea68893efffc58d3
Adrien Vanderpotte <adrien.vanderpotte@gmail.com>
2018-04-10 14:18:05 +0200

    :recycle: Game > Score > Clean component

--- src/scripts/components/game-score-manager/script.js
+++ src/scripts/components/game-score-manager/script.js
@@ -1,9 +1,4 @@
-// import eventHub from 'Application/event-hub'
-// import gameScoreManager from 'Game/gameScoreManager'
-
 import { INCREMENT_SCORE } from 'MutationTypes'
-
-console.log(INCREMENT_SCORE)
 
 export default
 {
@@ -17,7 +12,6 @@ export default
     data()
     {
         return {
-            // score: 0
         }
     },
 
@@ -30,30 +24,17 @@ export default
 
     created()
     {
-        // eventHub.$on('game:submit-answer', this.onNewAnswer)
     },
 
     mounted()
     {
-        // this.score = gameScoreManager.getScore()
     },
 
     destroyed()
     {
-        // eventHub.$off('game:submit-answer', this.onNewAnswer)
     },
 
     methods:
     {
-        onNewAnswer(win)
-        {
-            if(win)
-            {
-                this.$store.commit(INCREMENT_SCORE)
-                // this.$store.commit(SET_PROGRESS, )
-                // this.score++
-                // gameScoreManager.setScore(this.score)
-            }
-        }
     }
 }
