1f8d24016b474021c6a718003f3cd34d9dbd6de5
Antonin Riviere <antonin.rvr@gmail.com>
2018-04-11 15:52:29 +0200

    :contruction: The Crown > Add WIP animations

--- src/scripts/components/quiz-custom/the-crown-svg/script.js
+++ src/scripts/components/quiz-custom/the-crown-svg/script.js
@@ -29,7 +29,12 @@ export default
 
     mounted()
     {
-        console.log('mounted the crown svg')
+        this.corgi = {}
+        this.corgi.elem = this.$el.querySelector('.corgi svg'),
+        this.corgi.width = parseInt(window.getComputedStyle(this.corgi.elem, null).getPropertyValue('width'))
+        this.corgi.height = parseInt(window.getComputedStyle(this.corgi.elem, null).getPropertyValue('height'))
+
+        console.log('mounted the crown svg', this.corgi)
     },
 
     destroyed()
@@ -39,6 +44,9 @@ export default
 
     methods:
     {
-
+        onClick()
+        {
+            this.corgi.elem.style.transform = `translate3d(${this.$root.mouse.x - this.corgi.width / 2}px, ${this.$root.mouse.y - this.corgi.height / 2}px,0)`
+        }
     }
 }
